d66b2510bf07d74da532739d224ee85f3d290820
Spartak Maznytskyi <109909363+Spartakm67@users.noreply.github.com>
2023-03-20 12:51:55 +0200

    Commit 03/20/2023

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,20 @@
-// export const App = () => {
-//   return (
-//     <div
-//       style={{
-//         height: '100vh',
-//         display: 'flex',
-//         justifyContent: 'center',
-//         alignItems: 'center',
-//         fontSize: 40,
-//         color: '#010101'
-//       }}
-//     >
-//       React homework template
-//     </div>
-//   );
-// };
-import { useState, useMemo } from "react";
+export const App = () => {
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101'
+      }}
+    >
+      React homework template
+    </div>
+  );
+};
+// import { useState, useMemo, useEffect } from "react";
 
 //   export const App = ({ someProp }) => {
 //   const [planets, setPlanets] = useState(["Earth", "Mars", "Jupiter", "Venus"]);
@@ -39,28 +39,28 @@ import { useState, useMemo } from "react";
 //     </div>
 //   );
 // };
-export const App = ({ someProp }) => {
-  const [planets, setPlanets] = useState(["Earth", "Mars", "Jupiter", "Venus"]);
-  const [query, setQuery] = useState("");
-  const [clicks, setClicks] = useState(0);
+// export const App = ({ someProp }) => {
+//   const [planets, setPlanets] = useState(["Earth", "Mars", "Jupiter", "Venus"]);
+//   const [query, setQuery] = useState("");
+//   const [clicks, setClicks] = useState(0);
 
-  const filteredPlanets = useMemo(
-    () => planets.filter(planet => planet.includes(query)),
-    [planets, query],
-    console.log("Render was done")
-  );
+//   const filteredPlanets = useMemo(
+//     () => planets.filter(planet => planet.includes(query)),
+//     [planets, query],
+//     console.log("Render was done")
+//   );
  
-  return (
-    <div>
-      <div>Some prop: {someProp}</div>
-      <button onClick={() => setClicks(clicks + 1)}>
-        Number of clicks: {clicks}
-      </button>
-      <div>
-        {filteredPlanets.map(planet => (
-          <div key={planet}>{planet}</div>
-        ))}
-      </div>
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <div>Some prop: {someProp}</div>
+//       <button onClick={() => setClicks(clicks + 1)}>
+//         Number of clicks: {clicks}
+//       </button>
+//       <div>
+//         {filteredPlanets.map(planet => (
+//           <div key={planet}>{planet}</div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
